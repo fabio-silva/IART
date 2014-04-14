d3f4bbd33ca3947e0a4ac1a2c43347baceb5db25
Fabio Filipe Jesus Silva <fabio.silva93@hotmail.com>
2014-04-14 16:13:47 +0100

    data read

--- src/proj/Main.java
+++ src/proj/Main.java
@@ -1,82 +1,93 @@
 package proj;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.evaluation.NominalPrediction;
+import weka.classifiers.trees.J48;
+import weka.core.Attribute;
 import weka.core.FastVector;
+import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 
 public class Main {
-	
+
 	static ArrayList<Data> data;
-	
-	public static void main(String[] args) throws Exception {
-		
-		data = new ArrayList<Data>();
-		
-		File file = new File("teste.data");
-		Scanner s = new Scanner(file);
-		String line;
 
-		while (s.hasNextLine()) {
-			ArrayList<Boolean> values = new ArrayList<Boolean>();
-			line = s.nextLine();
-			String[] tokens = line.split("\\s");
-			for (int i = 1; i < tokens.length - 1; i++) {
-				if (tokens[i].equals("yes")) {
-					values.add(true);
-				} else if (tokens[i].equals("no")){
-					values.add(false);
-				}
-				else {
-					System.out.println("Unknown");
-				}
-			}
-			String number = tokens[0].replace(',', '.');
-			Data d = new Data(Float.parseFloat(number), values);
-			data.add(d);
+	public static BufferedReader readFile(String file) {
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(file));
+		} catch (FileNotFoundException ex) {
+			System.err.println("File not found !");
 		}
-		s.close();
 
-		/*
-		 * try { while ((line = reader.readLine()) != null) {
-		 * System.out.println(line); ArrayList<String> temp = new
-		 * ArrayList<String>(); String[] tokens = line.split("\\s");
-		 * temperatures.add(Integer.parseInt(tokens[0])); for (int i = 1; i <
-		 * tokens.length - 1; i++) { temp.add(tokens[i]); } values.add(temp); }
-		 * } catch (IOException e) { e.printStackTrace(); }
-		 */
+		return reader;
+	}
 
-		System.out
-				.println("Attributes:\na1 - Patient temperature\na2 - Occurrence of nausea\n"
-						+ "a3 - Lumbar pain\na4 - Urine pushing\na5 - Micturition pains\n"
-						+ "a6 - Burning of urethra, itch, swelling of urethra outlet\n"
-						+ "d1 - decision: Inflammation of urinary bladder\n"
-						+ "d2 - decision: Nephritis of renal pelvis origin");
+	public static Evaluation classification(Classifier model,
+			Instances training, Instances testing) throws Exception {
+		Evaluation ev = new Evaluation(training);
+		model.buildClassifier(training);
+		ev.evaluateModel(model, testing);
 
-		FastVector a2 = new FastVector(2);
-		a2.addElement("yes");
-		a2.addElement("no");
-		FastVector a3 = new FastVector(2);
-		a3.addElement("yes");
-		a3.addElement("no");
-		FastVector a4 = new FastVector(2);
-		a4.addElement("yes");
-		a4.addElement("no");
-		FastVector a5 = new FastVector(2);
-		a5.addElement("yes");
-		a5.addElement("no");
-		FastVector a6 = new FastVector(2);
-		a6.addElement("yes");
-		a6.addElement("no");
-		FastVector d1 = new FastVector(2);
-		d1.addElement("yes");
-		d1.addElement("no");
-		FastVector d2 = new FastVector(2);
-		d2.addElement("yes");
-		d2.addElement("no");
+		return ev;
+	}
+
+	public static double accuracyCalc(FastVector arr) {
+		double correct = 0;
+
+		for (int i = 0; i < arr.size(); i++) {
+			NominalPrediction prediction = (NominalPrediction) arr.get(i);
+
+			if (prediction.predicted() == prediction.actual())
+				correct++;
+		}
+
+		return 100 * correct / arr.size();
+	}
+
+	public static Instances[][] splits(Instances data, int splitsNo) {
+		Instances split[][] = new Instances[2][splitsNo];
+
+		for (int i = 0; i < splitsNo; i++) {
+			split[0][i] = data.trainCV(splitsNo, i);
+			split[1][i] = data.testCV(splitsNo, i);
+		}
+
+		return split;
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader file = readFile("teste.data");
+		Instances data = new Instances(file);
+		data.setClassIndex(data.numAttributes() - 1);
+
+		Instances[][] split = splits(data, 3);
+
+		Instances[] training = split[0];
+		Instances[] testing = split[1];
+
+		FastVector predictions = new FastVector();
+		Classifier classifier = new J48(); //C4.5
+		for (int i = 0; i < training.length; i++) {
+			Evaluation valid = classification(classifier, training[i],
+					testing[i]);
+			predictions.appendElements(valid.predictions());
+
+			System.out.println(classifier.toString());
+		}
+
+		double acc = accuracyCalc(predictions);
+		
+		System.out.println("Accuracy of " + classifier.getClass().getSimpleName() + ": " + String.format("%.2f%%", acc) + "\n----------------");
 	}
 }
